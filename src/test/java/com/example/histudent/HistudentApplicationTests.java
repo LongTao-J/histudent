79da1f33f8a995c16fe70bc296fd6a185e7f301a
MuShan-bit <77890771+MuShan-bit@users.noreply.github.com>
2022-07-19 11:50:11 +0800

    Initial commit

---
+++ src/test/java/com/example/histudent/HistudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.histudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
