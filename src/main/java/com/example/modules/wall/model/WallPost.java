f18fe987ac208d7e707e58ed62fe0ac966532dd3
MuShan-bit <77890771+MuShan-bit@users.noreply.github.com>
2022-07-23 22:27:17 +0800

    123

---
+++ src/main/java/com/example/modules/wall/model/WallPost.java
@@ -0,0 +1,14 @@
+package com.example.modules.wall.model;
+
+/**
+ * @author mushan
+ * @date 7/23/2022
+ * @apiNote
+ */
+public class WallPost {
+    Long id;
+    Long userId;
+    String title;
+    String content;
+
+}
