f4a7ef9152f3447fe583a371572191f5fe2a46d3
cannian-py <879142964@qq.com>
2022-07-24 21:06:27 +0800

    学校学院专业信息完成

--- src/main/java/com/example/modules/collegeInformation/service/SchoolService.java
+++
@@ -1,7 +0,0 @@
-package com.example.modules.collegeInformation.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.modules.collegeInformation.pojo.School;
-
-public interface SchoolService extends IService<School> {
-}
--- src/main/java/com/example/modules/collegeInformation/service/impl/SchoolServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.modules.collegeInformation.service.impl;
-
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.example.modules.collegeInformation.mapper.SchoolMapper;
-import com.example.modules.collegeInformation.pojo.School;
-import com.example.modules.collegeInformation.service.SchoolService;
-
-public class SchoolServiceImpl extends ServiceImpl<SchoolMapper, School> implements SchoolService {
-}
