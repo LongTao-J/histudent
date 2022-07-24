c69c55c2df7dace6ab50386b73c8a0245efcc406
cannian-py <879142964@qq.com>
2022-07-24 14:13:16 +0800

    test-luyu

--- src/test/java/com/example/HistudentApplicationTests.java
+++ src/test/java/com/example/HistudentApplicationTests.java
@@ -1,10 +1,6 @@
 package com.example;
 
-import com.example.modules.collegeInformation.mapper.SchoolMapper;
-import com.example.modules.collegeInformation.pojo.School;
-import com.example.modules.collegeInformation.service.SchoolService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
---
+++ src/test/java/com/example/modules/collegeInformation/Test.java
@@ -0,0 +1,7 @@
+package com.example.modules.collegeInformation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test {
+}
