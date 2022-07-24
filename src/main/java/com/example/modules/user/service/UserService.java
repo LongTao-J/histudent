be02e08eeab0ad51fd446bec58cc6539b79989a9
MuShan-bit <77890771+MuShan-bit@users.noreply.github.com>
2022-07-24 14:05:21 +0800

    更新

--- src/main/java/com/example/modules/user/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.modules.user.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.histudent.pojo.User;
-
-public interface UserService extends IService<User> {
-}
--- src/main/java/com/example/modules/user/utils/Consts.java
+++
@@ -1,6 +0,0 @@
-package com.example.modules.user.utils;
-
-public class Consts {
-    public static final String SESSION_USER="SessionUser";
-    public static final String SESSION_SMS="SessionSms";
-}
