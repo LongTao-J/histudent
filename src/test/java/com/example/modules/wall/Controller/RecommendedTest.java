e4730744ffa356c69d588ac19d476cdd3f905a03
MuShan-bit <77890771+MuShan-bit@users.noreply.github.com>
2022-08-03 16:24:35 +0800

    mushan

---
+++ src/test/java/com/example/modules/wall/Controller/RecommendedTest.java
@@ -0,0 +1,6 @@
+package com.example.modules.wall.Controller;/**
+* @author mushan
+* @date 8/3/2022
+* @apiNote
+*/public class RecommendedTest {
+}
---
+++ src/test/java/com/example/modules/wall/service/WallPostRecommendedTest.java
@@ -0,0 +1,24 @@
+package com.example.modules.wall.service;
+
+import com.example.modules.walls.service.WallPostRecommendedService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author mushan
+ * @date 8/3/2022
+ * @apiNote
+ */
+@SpringBootTest
+public class WallPostRecommended {
+    @Autowired
+    WallPostRecommendedService wallPostRecommendedServiceImpl;
+
+    @Test
+    void insertTest(){
+
+    }
+
+
+}
